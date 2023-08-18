baf512cc6cd77ecbf7b267fc011abd92997bf4b9
haeyng <haeyng.song@gmail.com>
2023-08-19 07:32:23 +0900

    단축키 추가, 오타 수정

--- Java/days01/Ex01.java
+++ Java/days01/Ex01.java
@@ -1,19 +1,35 @@
 package days01;
 
-public class Ex01 {  //SampleEX01 - 카멜 표기법
+public class Ex01 {  //SampleEx01 - 카멜 표기법
 	
 	public static void main(String[] args) {  //프로그램 시작
-		//이클립스에서 자주 사용하는 단축키 정리
-		//단축키 목록 : Ctrl + Shift + L
-		//실행 : Ctrl + f11
-		//자동완성 Ctrl + space
-		//들여쓰기 Ctrl + A, Ctrl + I
-		//단일행 주석처리 Ctrl + /
 		
-		System.out.println("송해영");
-		System.out.println("송해영");
-		System.out.println("송해영");
+		//[이클립스에서 자주 사용하는 단축키 정리]
 		
+		//단축키 전체 목록 확인 : Ctrl + Shift + L
+		
+		//실행 : Ctrl + F11
+		//디버깅할 때 실행 : F11
+		
+		//자동완성 : Ctrl + space
+		//자동 import : Ctrl + Shift + O
+		//들여쓰기 : Ctrl + A, Ctrl + I
+		//한 줄 삭제 : Ctrl + D
+		//한 줄 복사 : Ctrl + Alt + ↑↓방향키
+		//한 줄 바꾸기 : ↑↓방향키
+		//현재 커서부터 끝까지 블록 설정 : Shift + End
+		//현재 커서부터 처음까지 블록 설정 : Shift + Home	
+		
+		//단일행 주석처리,해제 Ctrl + /
+		//블록 주석처리,해제 : Ctrl + Shift + /
+		
+		//이전 작업 위치로 이동 : Alt + ← 방향키
+		//다음 작업 위치로 이동 : Alt + → 방향키 
+		//클래스 선택 후 F4 상속 계층 확인
+		//변수, 메서드 정의 이동 : F3
+		
+		System.out.println("홍길동");
+		System.out.println("김길동");		
 		
 	}//main 닫기 - 프로그램 종료
 
